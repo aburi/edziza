d652875c35ab2fa5179a3869fea9db7d0082d19b
aburi <alex@alexburi.com>
2016-06-04 15:24:40 -0700

    Design work

--- site/web/app/themes/sage/templates/header.php
+++ site/web/app/themes/sage/templates/header.php
@@ -1,14 +1,14 @@
-<header class="banner m-t-3 m-b-0">
+<header class="banner m-t-3 m-b-1 clearfix">
   <div class="container">
     <a class="navbar-brand" href="<?= esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
     <nav class="navbar navbar-light bg-faded pull-xs-right">
       <?php
       if ( has_nav_menu( 'primary_navigation' ) ) :
-        wp_nav_menu( [
+        wp_nav_menu( array(
           'theme_location' => 'primary_navigation',
           'menu_class'     => 'nav navbar-nav',
-          'container'      => false
-        ] );
+          'container'      => false,
+        ) );
       endif;
       ?>
     </nav>
