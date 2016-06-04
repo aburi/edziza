cf7aae77b5b86b31564f08c5ba3e9016997b3cd0
aburi <alex@alexburi.com>
2016-06-04 01:10:13 -0700

    Add base design

--- site/web/app/themes/sage/templates/header.php
+++ site/web/app/themes/sage/templates/header.php
@@ -1,10 +1,14 @@
-<header class="banner">
+<header class="banner m-t-3 m-b-0">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <a class="navbar-brand" href="<?= esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+    <nav class="navbar navbar-light bg-faded pull-xs-right">
       <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      if ( has_nav_menu( 'primary_navigation' ) ) :
+        wp_nav_menu( [
+          'theme_location' => 'primary_navigation',
+          'menu_class'     => 'nav navbar-nav',
+          'container'      => false
+        ] );
       endif;
       ?>
     </nav>
